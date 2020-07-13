31926f8e7927e53a6ae07051a15e17b24cfbfef5
Toby Cox <toby@dovetailstudios.com>
2020-07-13 20:35:50 +1200

    Initial commit with another component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
+import Images from './Images.js';
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(async () => {
+    const result = await axios(
+      'https://hn.algolia.com/api/v1/search?query=redux',
+    );
+
+    setData(result.data);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Images images={data} />
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Images(images) {
+  return (
+    <div className="App">
+      Images
+    </div>
+  );
+}
+
+export default Images;
